62065b01a74b789b46c774406547b5cc481517cd
Carlos Gonzalez <xastherion@users.noreply.github.com>
2018-04-26 17:38:25 +0200

    a simple macos installer script for wordgrinder

---
+++ wordgrinder_installer.sh
@@ -0,0 +1,31 @@
+# xcode intallation --> ask for accept (please accept!) 
+# ----------------------------------------------------------------------
+xcode-select --install
+
+# homebrew installation --> ask for sudoer password (please give it!
+# ---------------------------------------------------------------------- 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# ninja installation
+# ----------------------------------------------------------------------
+brew install ninja
+
+# install wordgrinder 
+# ----------------------------------------------------------------------
+git clone https://github.com/davidgiven/wordgrinder.git
+cd wordgrinder
+make
+make install
+rm -Rf wordgrinder
+
+# put a beautiful clickable icon in desktop:
+# -----------------------------------------------------------------------
+echo '#!/bin/bash'>~/Desktop/wordgrinder.command
+echo '~/bin/wordgrinder'>>~/Desktop/wordgrinder.command
+chmod +x ~/Desktop/wordgrinder.command
+
+# for maximal enjoy, put the wordgrinder icon in your .command
+# go to http://cowlark.com/sky-cow.png
+# copy image
+# go to wordgrinder.command and show "properties"
+# then click on icon and paste!
